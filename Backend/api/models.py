3b51765f4d476ee23a269e740d96f2b37d3014fb
Risvini19 <risvini2001@gmail.com>
2025-03-18 14:46:01 +0530

    updated models

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.models import User
-from django.db import models
+from django.contrib import admin
+from django.urls import path, include
 
-class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    data = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class Donation(models.Model):
-    donor = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    donated_at = models.DateTimeField(auto_now_add=True)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
